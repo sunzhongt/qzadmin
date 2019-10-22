86519dba765b28147d57f1b2621b00ad03aabaa2
suznhongt <2268844039@qq.com>
2019-10-22 17:30:46 +0800

    弹框

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.702f930418b41d59cf20b3804045e928.js"
+  "./precache-manifest.e1fdc0bec8422cacf8a93b25696ddbee.js"
 );
 
 self.addEventListener('message', (event) => {
